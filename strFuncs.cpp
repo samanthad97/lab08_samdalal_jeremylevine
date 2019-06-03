9208ced41beed808e4783662272c250f75b93e56
Samantha Dalal <samantha_dalal@ucsb.edu>
2019-06-02 21:39:23 -0700

    test anagram function

--- strFuncs.cpp
+++ strFuncs.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <cassert>
 #include <algorithm>
 #include "strFuncs.h"
 using namespace std;
@@ -12,6 +13,28 @@ using namespace std;
  * affect your result. 
  */
 bool isAnagram(string s1, string s2){
+  int count1[256];
+  int count2[256];
+
+  for(int i = 0; i < 256; i++){
+    count1[i] = 0;
+    count2[i] = 0;
+  }
+
+  for(int i = 0; i < strlen(s1); i++){
+    count1[tolower(string1[i])-0]++;
+  }
+
+  for(int i = 0; i < strlen(s2); i++){
+    count2[tolower(string2[i])-0]++;
+  }
+
+  for(int i = 97; i < 123; i++){
+    if(count1[i] != count2[i]){
+      return false;
+    }
+  }
+
   return true;
 }
 
